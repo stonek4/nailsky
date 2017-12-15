7ac64644f95e221c16b5cc8cd663defd0da6fea5
Kevin Stoner <stonek4rpi@gmail.com>
2017-12-14 21:12:23 -0700

    made table on tabbed panel fade

--- src/tabbed-panel/tabbed-panel.ts
+++ src/tabbed-panel/tabbed-panel.ts
@@ -3,18 +3,6 @@ import { Row } from '../time-table/time-table';
 
 export class TabbedPanel {
     @bindable public tabs: Tab[];
-    public headers: string[] = new Array<string>();
-    public rows: Row[];
-
-    public changeTable(tab: Tab) {
-        this.headers = tab.headers;
-        this.rows = tab.rows;
-    }
-
-    public attached() {
-        this.headers = this.tabs[0].headers;
-        this.rows = this.tabs[0].rows;
-    }
 }
 
 export class Tab {
